9601140db805f32f4aa1649adfcc902721887269
maartyna-xyz <maartyna-xyz@users.noreply.github.com>
2020-04-13 18:39:41 +0200

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    
+/* --- sticky navigation --- */
+    
+    $('.js--section-about').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '95%;'
+    });
+    
+    
+/* --- main CTA scroll --- */
+    
+    $('.js--scroll-to-offer').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-offer').offset().top}, 1000); 
+    });
+    
+    
+/* --- scroll in NAV --- */
+    
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    
+    
+/* --- mobile NAV --- */
+    
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else { 
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+    
+    
+});
